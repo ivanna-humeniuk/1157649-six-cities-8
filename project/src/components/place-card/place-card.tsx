ed87b4a3977de24d45bca2acc8afea1bbaef32a0
ivanna-humeniuk <gumenuk777.ivanna@gmail.com>
2021-10-19 10:09:56 +0300

    Added classnames

--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,5 +1,6 @@
 import {useCallback, useMemo} from 'react';
 import {Link} from 'react-router-dom';
+import classnames from 'classnames';
 import {Offer} from '../../types/offers';
 
 type PlaceCardProps = {
@@ -21,10 +22,9 @@ function PlaceCard(props: PlaceCardProps): JSX.Element {
       return handleHoverOn(place.id);
     }
   }, [handleHoverOn, place.id]);
-
   return (
     <article
-      className={`${cardClasses.article} place-card`}
+      className={classnames(cardClasses.article, 'place-card')}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleHoverOff}
     >
@@ -33,12 +33,12 @@ function PlaceCard(props: PlaceCardProps): JSX.Element {
           <span>Premium</span>
         </div>
       )}
-      <div className={`${cardClasses.imageWrapper} place-card__image-wrapper`}>
+      <div className={classnames(cardClasses.imageWrapper, 'place-card__image-wrapper')}>
         <Link to={`/offer/${place.id}`}>
           <img className="place-card__image" src={place.previewImage} width="100%" height="100%" alt="Place apartment"/>
         </Link>
       </div>
-      <div className={`${cardClasses.info ? cardClasses.info : ''} place-card__info`}>
+      <div className={classnames(cardClasses.info, 'place-card__info')}>
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
             <b className="place-card__price-value">&euro;{place.price}</b>
